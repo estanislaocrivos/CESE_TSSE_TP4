3a4164924dc3b5c1f9e4ed39f593601da1ed8ebe
Estanislao Crivos <estanislaocrivos@gmail.com>
2024-03-21 20:51:29 -0300

    Primera versión del repositorio.

---
+++ src/main.c
@@ -0,0 +1,72 @@
+/************************************************************************************************
+Copyright (c) 2023, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/** @file main.c
+ ** @brief Definición de la función principal del programa
+ **/
+
+/* === Headers files inclusions =============================================================== */
+
+#include "main.h"
+#include "gpio.h"
+
+/* === Macros definitions ====================================================================== */
+
+#define GPIO_A 1
+#define GPIO_B 2
+#define PIN_3  3
+#define PIN_5  5
+
+/* === Private data type declarations ========================================================== */
+
+/* === Private variable declarations =========================================================== */
+
+/* === Private function declarations =========================================================== */
+
+/* === Public variable definitions ============================================================= */
+
+/* === Private variable definitions ============================================================ */
+
+/* === Private function implementation ========================================================= */
+
+void Delay(void) {
+}
+/* === Public function implementation ========================================================== */
+
+int main(void) {
+    gpio_t led_rojo;
+    gpio_t led_verde;
+
+    led_rojo = GpioCreate(GPIO_A, PIN_3);
+    led_verde = GpioCreate(GPIO_B, PIN_5);
+
+    GpioSetDirection(led_rojo, true);
+    GpioSetState(led_rojo, true);
+    while (1) {
+        GpioSetState(led_rojo, false);
+        Delay();
+        GpioSetState(led_rojo, true);
+    }
+
+    return 0;
+}
+
+/* === End of documentation ==================================================================== */
